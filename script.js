c6f73500d4a49244b9d8f3c69d2bedd5a14fbc39
Optima 3D Care <151742282+Optima3DCare@users.noreply.github.com>
2023-12-05 19:03:52 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,54 @@
+const menuIcon = document.querySelector('#menu-icon');
+const navbar = document.querySelector('.navbar');
+const navbg = document.querySelector('.nav-bg');
+menuIcon.addEventListener('click', () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+    navbg.classList.toggle('active');
+});
+
+VanillaTilt.init(document.querySelectorAll(".card"), {
+    max: 25,
+    speed: 100,
+glare:true,
+"max-glare":1,
+});
+
+VanillaTilt.init(document.querySelectorAll(".testimonial"), {
+    max: 10,
+    speed: 100,
+glare:true,
+"max-glare":1,
+});
+
+const close = document.querySelector(".window__close")
+close.addEventListener('click', () => {
+    document.querySelector(".window").style.display = 'none'
+    document.querySelector(".rakshak").textContent = "Click me to get it back!"
+    document.querySelector(".rakshak").style.cursor = 'pointer'
+    document.querySelector(".rakshak").addEventListener("click",()=>{
+        document.querySelector(".window").style.display = 'block'
+        document.querySelector(".rakshak").textContent = "Introducing InsuRakshak"
+    })
+})
+
+const thums = document.querySelectorAll("#thums")
+thums.forEach((elem) =>{
+    elem.addEventListener("click",() =>{ 
+        elem.textContent = "💙"
+    })
+})
+
+const wow = document.querySelectorAll(".wow")
+wow.forEach(elem => {
+    elem.addEventListener("click",() => {
+        elem.textContent = "😁💙"
+    })
+})
+
+const click = document.querySelector(".explore_click")
+const explore = document.querySelector(".explore")
+explore.addEventListener("click", () => {
+    click.click();
+})
+
